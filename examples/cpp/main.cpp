d30874a5051bd8548664c10e9dbe7310e8c1581a
dtor <danieltorres@outlook.pt>
2024-11-11 19:04:20 +0000

    examples: add suggested fix for clang-tidy misc check

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -1,8 +1,7 @@
 #include "hello.hpp"
 #include <iostream>
-#include <string>
 
-auto main(int argc, char** argv) -> int {
+auto main(int  /*argc*/, char**  /*argv*/) -> int {
   std::cout << greet() << '\n';
   return 0;
 }
