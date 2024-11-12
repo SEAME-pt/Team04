c254c40675fe7813b4b5370a1984fc4de2cce2d7
dtor <danieltorres@outlook.pt>
2024-11-12 09:55:43 +0000

    bazel: add support for qt
    Make qt hermetic and add hello world that opens up a window

---
+++ examples/qt/hello.hpp
@@ -0,0 +1,13 @@
+#include <qwidget.h>
+#include <QWidget>
+
+class MyWidget : public QWidget
+{
+public:
+    MyWidget();
+    ~MyWidget() override;
+    MyWidget(const MyWidget&) = delete;
+    auto operator=(const MyWidget&) -> MyWidget& = delete;
+    MyWidget(MyWidget&&) = delete;
+    auto operator=(MyWidget&&) -> MyWidget& = delete;
+};
