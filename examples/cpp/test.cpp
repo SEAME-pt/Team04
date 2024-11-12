e8dd4d228b4c42c07fa38b69cfc64cdf99275301
dtor <danieltorres@outlook.pt>
2024-11-12 23:07:03 +0000

    bazel: change from gcc to clang as default compiler

--- examples/cpp/test.cpp
+++ examples/cpp/test.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include "hello.hpp"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wglobal-constructors"
 TEST(HelloWorldTest, GreetTest) {
+#pragma GCC diagnostic pop
     EXPECT_EQ(greet(), "Hello World!");
 }
