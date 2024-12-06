08aa053efc5804b0b5f4e47c5ebebc6ca51e50f6
Thais Silva <tha.sribeiro@gmail.com>
2024-12-06 12:49:22 +0000

    gui:add basic speed display

---
+++ instrument-cluster/speed.h
@@ -0,0 +1,12 @@
+#ifndef SPEED_H
+#define SPEED_H
+class Speed {
+   private:
+    double current;
+   public:
+    Speed(double initial);
+    void increase();
+    void decrease();
+    double getCurrent();
+};
+#endif  // SPEED_H
