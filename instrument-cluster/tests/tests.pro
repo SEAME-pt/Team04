45ace5a95c3267b6fad4e57150bca09641c4672f
Thais <tha.sribeiro@gmail.com>
2024-12-18 16:36:07 +0000

    gui: add unit tests

--- instrument-cluster/tests/tests.pro
+++ instrument-cluster/tests/tests.pro
@@ -7,19 +7,24 @@ CONFIG += console c++20
 CONFIG -= app_bundle
 CONFIG += thread
 CONFIG -= qt
+CONFIG += moc
+
 
 SOURCES += \
         main.cpp \
         tst_basicdisplay.cpp \
         tst_rpm.cpp \
-        tst_speed.cpp
+        tst_speed.cpp \
+        tst_zmqsubscriber.cpp
 
 INCLUDEPATH += $$PWD/..
 
 SOURCES += ../speed.cpp \
-           ../rpm.cpp
+           ../rpm.cpp \
+           ../zmqsubscriber.cpp
 HEADERS += ../speed.h \
-           ../rpm.h
+           ../rpm.h \
+           ./zmqsubscriber.h
 
 # Google Test
 INCLUDEPATH += /usr/include/gtest
---
+++ instrument-cluster/tests/tst_zmqsubscriber.cpp
@@ -0,0 +1,50 @@
+#include "zmqsubscriber.h"
+#include <gtest/gtest.h>
+#include <QObject>
+
+class TestZmqSubscriber : public ::testing::Test {
+   protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+TEST_F(TestZmqSubscriber, DecodeMessageTest) {
+    ZmqSubscriber subscriber("ipc://test");
+
+    uint8_t receivedSpeed = 0;
+    uint16_t receivedRpm = 0;
+
+    QObject::connect(&subscriber, &ZmqSubscriber::messageReceived, [&](uint8_t speed, uint16_t rpm) {
+        receivedSpeed = speed;
+        receivedRpm = rpm;
+    });
+
+            // Simular uma mensagem válida
+    uint8_t messageData[4] = {100, 0, 0x70, 0x17}; // Speed = 100, RPM = 6000
+    zmq::message_t message(messageData, 4);
+
+    subscriber.checkForMessages(); // Força o processamento da mensagem
+
+    EXPECT_EQ(receivedSpeed, 100);
+    EXPECT_EQ(receivedRpm, 6000);
+}
+
+TEST_F(TestZmqSubscriber, InvalidMessageTest) {
+    ZmqSubscriber subscriber("ipc://test");
+
+    bool signalTriggered = false;
+
+    QObject::connect(&subscriber, &ZmqSubscriber::messageReceived, [&](uint8_t, uint16_t) {
+        signalTriggered = true;
+    });
+
+    uint8_t invalidData[2] = {50, 0}; // Mensagem inválida
+    zmq::message_t invalidMessage(invalidData, 2);
+
+    subscriber.checkForMessages(); // Não deve emitir sinal
+
+    EXPECT_FALSE(signalTriggered);
+}
