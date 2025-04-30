33db261d8b10366fe1266ec9d9b1b09eee1a05e2
joao <joao.alves@seame.pt>
2025-04-30 18:09:32 +0100

    car control: initial test of Jetson Raspberry communication

---
+++ examples/sendMove/sendMove.py
@@ -0,0 +1,14 @@
+import zmq
+import struct
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://10.21.221.30:5555")
+
+# Send command: acceleration 0.5 (50% forward), steering -0.2 (20% left)
+data = struct.pack("ff", 0.5, -0.2)
+socket.send(data)
+
+# Wait for response
+response = socket.recv()
+print(f"Response: {response}")
